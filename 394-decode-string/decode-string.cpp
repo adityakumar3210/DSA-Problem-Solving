8dd3750093eea0ab06c0bdb7d2d040c2b14dd6ee
adityakumar3210 <adityakumar20017558@gmail.com>
2026-08-14 14:33:16 +0530

    Time: 2 ms (23.79%) | Memory: 9.5 MB (35.43%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        
+        int n = s.length();
+
+        stack<string> st;
+        stack<int>    num;
+
+        for(int i=0; i<n; i++) {
+
+            
+            if(s[i] >= '0' && s[i] <= '9') {
+
+                if(i+2 < n && (s[i+1] >= '0' && s[i+1] <= '9' && s[i+2] >= '0' && s[i+2] <= '9')) {
+
+                    num.push(stoi(s.substr(i, 3)));
+                    i = i+2;
+                }
+                else if(i+1 < n &&  s[i+1] >= '0' && s[i+1] <= '9') {
+                    num.push(stoi(s.substr(i, 2)));
+                    i = i+1;
+                }
+                else{
+                    num.push(s[i] - '0');
+                }
+            }
+            else if(s[i] == '[' || s[i] >= 'a' && s[i] <= 'z') {
+                st.push(string(1, s[i]));
+
+            }
+            else{
+
+                string temp = "";
+
+                while(st.top() != "[") {
+
+                    temp = st.top() + temp;
+                    st.pop();
+
+                }
+                st.pop();
+                int k = num.top(); num.pop();
+
+                string ans = "";
+
+                for(int i=0; i<k; i++) {
+                    ans += temp;
+                }
+
+                st.push(ans);
+            }
+        }
+
+        string result = "";
+
+        while(!st.empty()) {
+
+            result = st.top() + result;
+            st.pop();
+        }
+
+        return result;
+    }
+};
