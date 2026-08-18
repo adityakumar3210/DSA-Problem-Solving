8c404d7178e38e9dbe37d7848a14c669d6e3d679
adityakumar3210 <adityakumar20017558@gmail.com>
2026-08-18 16:12:52 +0530

    Time: 11 ms (47.94%) | Memory: 11.8 MB (82.15%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
+    int n,
+        total;
+    vector<int> prefix;
+
+    int t[101][101];
+
+    int solve(int idx, int M, vector<int>& piles) {
+
+        if(idx == n) 
+            return 0;
+
+        if(t[idx][M] != -1) 
+            return t[idx][M];
+
+        int result = 0;
+        int stones = 0;
+
+        for(int X=1; X <= min(2*M, n-idx); X++) {
+            
+            int i = idx + X - 1;
+            stones += piles[i];
+            int remaining = total - prefix[i+1];
+
+            int alice_take = stones + remaining -  solve(i+1, max(X, M), piles);
+
+            result = max(alice_take, result);
+        }
+
+        return t[idx][M] = result;
+    }
     int stoneGameII(vector<int>& piles) {
         
-        int n = piles.size();
-        
-        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
-        vector<int> suffixSum(n, 0);
-        suffixSum[n - 1] = piles[n - 1];
-        
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + piles[i];
-        }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            for (int m = 1; m <= n; m++) {
-                if (i + 2 * m >= n) {
-                    dp[i][m] = suffixSum[i];
-                } else {
-                    for (int x = 1; x <= 2 * m; x++) {
-                        dp[i][m] = max(dp[i][m], suffixSum[i] - dp[i + x][max(m, x)]);
-                    }
-                }
-            }
-        }
-        
-        return dp[0][1];
-        
+        n = piles.size();
+
+        prefix.resize(n+1, 0);
+
+        for(int i=1; i<=n; i++) 
+           prefix[i] = piles[i-1] + prefix[i-1];
+
+        total = prefix[n];
+
+        memset(t, -1, sizeof(t));
+
+        return solve(0, 1, piles);
     }
 };
