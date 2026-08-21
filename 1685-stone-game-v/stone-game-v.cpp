a91a5a44f89f2198aad6a6726637788368720cc6
adityakumar3210 <adityakumar20017558@gmail.com>
2026-08-21 16:28:02 +0530

    Time: 286 ms (70.31%) | Memory: 14.1 MB (97.84%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -3,50 +3,43 @@ public:
     int t[501][501];
     int solve(int s, int e, int total, vector<int>& stoneValue) {
 
-        if(s == e) 
-            return 0;
+        if(s > e) 
+           return 0;
 
         if(t[s][e] != -1) 
-            return t[s][e];
+           return t[s][e];
 
-        int result = INT_MIN;
-        int score  = 0;
-        int sum    = 0;
+        int result = 0,
+            sum    = 0;
 
-        for(int i=s; i<e; i++) {
+        for(int X=1; X <= e-s+1; X++) {
+            int i = X+s-1;
+            sum +=  stoneValue[i];
 
-            sum += stoneValue[i];
-            int right = total - sum;
+            int reamaing = total - sum;
 
-            if(right > sum) {
-                
-                score = sum + solve(s, i, sum, stoneValue);
-                result = max(score, result);
-                
+            if(reamaing > sum) {
+                // left part
+                result = max(result, sum + solve(s, i, sum,  stoneValue));
             }
-            else if(right < sum){
-                score = right + solve(i+1, e, right, stoneValue);
-                result = max(score, result);
-                 
-            }  
-            else{  
-            
-                result = max(result, max(sum + solve(s, i, sum, stoneValue), right + solve(i+1, e, right, stoneValue)));
-
-            }  
-
-        }  
+            else if(sum > reamaing) {
+                // right part
+                result = max(result , reamaing + solve(i+1, e, reamaing,  stoneValue));
+            }
+            else{
+                // Equal 
+                result = max(result , sum + max(solve(s, i, sum,  stoneValue), solve(i+1, e, reamaing,  stoneValue)));
+            }
+        }
 
         return t[s][e] = result;
-
-        
     }
     int stoneGameV(vector<int>& stoneValue) {
         
         int n = stoneValue.size();
         int total = accumulate(stoneValue.begin(), stoneValue.end(), 0);
 
-        memset(t,-1, sizeof(t));
+        memset(t, -1, sizeof(t));
 
         return solve(0, n-1, total, stoneValue);
     }
